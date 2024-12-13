0f157c18c0f6b426ed7bde2975ab3ad98531aed7
Dreidelx <javierz.gallardov@gmail.com>
2024-12-12 23:55:26 -0300

    Update estadistica_moderna.py

--- estadistica_moderna.py
+++ estadistica_moderna.py
@@ -9,10 +9,9 @@ def moda(datos):
         else:
             frecuencias[dato] = 1
     
-    # Encontrar la frecuencia máxima
     max_frecuencia = max(frecuencias.values())
     
-    # Obtener los valores con la frecuencia máxima
+    #Obtiene los valores con la frecuencia máxima
     modas = [key for key, value in frecuencias.items() if value == max_frecuencia]
     
     return modas
@@ -63,7 +62,7 @@ def cuartil(datos, pos):
     if posicion >= len(datos_ordenados):
         posicion = len(datos_ordenados) - 1
 
-    # Obtener el valor en esa posición
+    #Obtiene el valor en esa posición
     valor = datos_ordenados[posicion]
     
     return valor
@@ -73,18 +72,17 @@ def cof_re(x, y):
     if len(x) != len(y):
         raise ValueError("Las listas deben tener la misma longitud.")
     
-    # Calcular las medias de x y y
     media_x = sum(x) / len(x)
     media_y = sum(y) / len(y)
     
-    # Calcular las sumas de productos de las desviaciones de cada variable
+    # Calcula las sumas de productos de las desviaciones de cada variable
     suma_producto = sum((xi - media_x) * (yi - media_y) for xi, yi in zip(x, y))
     
-    # Calcular las sumas de los cuadrados de las desviaciones de cada variable
+    # Calcula las sumas de los cuadrados de las desviaciones de cada variable
     suma_cuadrado_x = sum((xi - media_x) ** 2 for xi in x)
     suma_cuadrado_y = sum((yi - media_y) ** 2 for yi in y)
     
-    # Calcular el coeficiente de correlación
+    # Calcula el coeficiente de correlación
     r = suma_producto / ( (suma_cuadrado_x * suma_cuadrado_y) ** 0.5 )
     
     return r
